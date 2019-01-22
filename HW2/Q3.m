3418319880bcf1db20392b2d00811e9cce53c123
Jonathan Wapman <jdwapman@ucdavis.edu>
2019-01-21 20:20:49 -0800

    Finished Q3

--- HW2/Q3.m
+++ HW2/Q3.m
@@ -6,122 +6,206 @@ function main
     
     %% Set up problem parameters
     
-    prob.rentProfit = 10;  % Amount of money Jack makes per car rental
-    prob.costPerCarMoved = 2;  % Cost to move car from location 1 to 2
-    prob.meanRequests = [3; 4];  % Average number of requests in the morning
-    prob.meanReturns = [3; 2];  % Average number of returns at the end of the day
-    prob.maxCars = 20;  % Maximum number of cars a location can support
-    prob.maxCarsMoved = 5;  % Maximum number of cars that can be moved each night
-    prob.discountRate = 0.9;
-
-    % Used to store the policy of how many cars should be moved to/from
-    % each location at the end of the night
-    prob.values = zeros(prob.maxCars+1, prob.maxCars+1);
-    prob.policies = zeros(prob.maxCars+1, prob.maxCars+1);
+    % Note: p is a structure to store the problem parameters, to make it
+    % easier to pass many values to the various functions
     
-%% Implement reinforcement learning
-    for itrCount = 1:1:1
-
-        % Policy evaluation
-        while true
-            delta = 0;  % Used to check whether the values have converged 
-
-            % Iterate over all states
-            for i = 0:1:prob.maxCars
-                for j = 0:1:prob.maxCars
-
-                    % Note: for values and policies, must add +1 to index
-                    v = prob.values(i+1, j+1);  % Get current value for the current state
-
-                    % Get the state value using the Bellman equation
-                    V = Bellman(prob, [i;j], prob.policies(i+1,j+1)); 
-                    
-                    prob.values(i+1,j+1)=V;  % Save to updated state values
-                    delta = max([delta, abs(v-V)]);
-                end
-            end
-
-            % Check whether the values have converged
-            if delta < 1e-3
-               delta
-               prob.values
-               break 
-            end 
+    % Maximum number of cars that can be at a location
+    p.maxCars = 20;
+    
+    % Income per car rented
+    p.rentIncome = 10;
+    
+    % Maximum number of cars that can be moved overnight
+    p.maxCarsMoved = 5;
+    
+    % Cost to move car between locations
+    p.costPerCarMoved = 2;
+    
+    % Lamba paramter for a poisson distribution
+    p.meanRequests1 = 3;
+    p.meanRequests2 = 4;
+    
+    % Lambda parameter for a poisson distribution
+    p.meanReturns1 = 3;
+    p.meanReturns2 = 2;
+    
+    p.discountRate = 0.9;
+    
+    % Possible actions
+    p.actions = -p.maxCarsMoved:1:p.maxCarsMoved;
+    
+    % Poisson probability of requesting a combination of cars
+    p.requestProbs = zeros(p.maxCars+1, p.maxCars+1);
+    
+    % Poisson probability of returning a combination of cars
+    p.returnProbs = zeros(p.maxCars+1, p.maxCars+1);
+    
+    values = zeros(p.maxCars+1, p.maxCars+1);  % State values
+    policy = zeros(p.maxCars+1, p.maxCars+1);  % Policy values
+    
+    %% Precompute Poisson Probabilities
+    for i = 0:1:p.maxCars
+        for j = 0:1:p.maxCars
+            requestProb = poisspdf(i, p.meanRequests1) * poisspdf(j, p.meanRequests2);
+            p.requestProbs(i+1,j+1) = requestProb;
+            
+            returnProb = poisspdf(i, p.meanReturns1) * poisspdf(j, p.meanReturns2);
+            p.returnProbs(i+1,j+1) = returnProb;
         end
+    end
+    
+    %% Implement reinforcement learning   
+    for itrCounter = 0:1:5
         
+        %  Plot results 
+        % Policy plot
+        figure
+        x = 0:1:p.maxCars;
+        y = 0:1:p.maxCars;
+        levels = -p.maxCarsMoved:1:p.maxCarsMoved;
+        [C,h] = contourf(x, y, policy, levels);
+        colorbar()
+        xlabel("# Cars at Second Location")
+        ylabel("# Cars at First Location")
+        title(strcat('\pi_', string(itrCounter)))
+        clabel(C,h);
+        
+        % Policy evaluation
+        while 1
+            delta = 0;  % Used to check whether the states have converged
+
+           % Iterate over all states
+           for i = 0:1:p.maxCars
+               for j = 0:1:p.maxCars
+                   
+                   % Note: for values and policies, must add +1 to index
+                   v = values(i+1,j+1);  % Get the current value for the current state
+
+                   % Set the state value using the Bellman equatio
+                   V = Bellman(p, values, [i,j], policy(i+1,j+1));
+
+                   values(i+1, j+1) = V;  % Save the updated state values
+                   delta = max(delta, abs(v-V));
+               end
+           end
+
+           if delta < 1e-3
+              break 
+           end
+
+        end
+
         % Policy improvement
-        policy_stable = 1;
+        policy_stable = 1;  % Initialization
         
-        % For each s in S
-        for i=0:1:prob.maxCars
-            for j=0:1:prob.maxCars
-                old_action = prob.policies(i+1,j+1);
+        % Iterate over all states
+        for i = 0:1:p.maxCars
+            for j = 0:1:p.maxCars
+                action_returns = [];
+
+                old_policy = policy(i+1,j+1);
                 
-                % Get new action
-                actionVals = [];
-                
-                % Valid actions based on present cars
-                possibleActions = [-min(i,prob.maxCarsMoved):1:min(j,prob.maxCarsMoved)];  
-                
-                % Check all actions
-                for a = possibleActions
-                    pi_s = Bellman(prob, [i;j], a);
-                    actionVals = [actionVals; pi_s];
+                % Determines which actions are valid based on the number of
+                % cars at each location.
+                validActions = -min(j,p.maxCarsMoved):...
+                                1:min(i, p.maxCarsMoved);
+                            
+                % Loop over all actions            
+                for a = validActions
+                    retval = Bellman(p, values, [i,j], a);
+                    action_returns = [action_returns; retval];
                 end
                 
-                % Find maximum argument
-                [argMax, argVal] = max(actionVals);
-                
-                new_action = possibleActions(argVal);
-                
-                prob.policies(i+1,j+1) = new_action;
-                
-                if new_action ~= old_action
+                % Determine the best action (which gives the maximum state
+                % value
+                [argVal, argMax] = max(action_returns);
+                new_policy = validActions(argMax);
+                policy(i+1,j+1) = new_policy;
+
+                % Check whether the policy is stable
+                if new_policy ~= old_policy
                    policy_stable = 0; 
                 end
-                
             end
         end
         
-        if policy_stable
-           prob.values
-           prob.policies
-           return
+        if itrCounter == 4
+            % Plot values at the fourth (final) iteration
+            figure
+            [xx, yy] = meshgrid(0:1:p.maxCars, 0:1:p.maxCars);
+            mesh(xx,yy,values);
+            title("v_\pi_4");
+            xlabel("# Cars at First Location");
+            ylabel("# Cars at Second Location"); 
         end
-
-    end
+    end   
+    
+    % Save results
+    save('results.mat')
+    
 end
 
-function[value] = Bellman(problem, state, action)
+function[returns] = Bellman(p, values, state, action)
+    returns = 0;  % Initialize to 0
     
-    % Next state is the current state plus the expected number of returns,
-    % then changed by the number of cars transferred
+    % Cost for car transfer
+    loss = abs(action) * p.costPerCarMoved;
     
-    nextState = state;
-    nextState = nextState + problem.meanReturns;
-    nextState(1) = nextState(1) + action;  % Move cars
-    nextState(2) = nextState(2) - action; 
+    returns = returns - loss;
     
-    if any(nextState <= 0)
-        nextState
-    end
-    
-    % Saturate to maxCars
-    nextState(nextState > problem.maxCars) = problem.maxCars;
+    % Go through all possible rental requests
+    for meanRequests1 = 0:1:p.maxCars
+        for meanRequests2 = 0:1:p.maxCars
+            
+            % Probability for the given combination of rental requests at
+            % the two locations
+            requestProb = p.requestProbs(meanRequests1+1, meanRequests2+1);
+       
+            % Moving cars
+            numCars1 = min(state(1) - action, p.maxCars);
+            numCars2 = min(state(2) + action, p.maxCars);
+            
+            % Saturate the number of requests to the number of cars
+            % actually available
+            requests1 = min(numCars1, meanRequests1);
+            requests2 = min(numCars2, meanRequests2);
+            
+            % Calculate the income from renting the cars
+            income = (requests1 + requests2) * p.rentIncome;
+            numCars1 = numCars1 - requests1;
+            numCars2 = numCars2 - requests2;
+            
+%             for returns1 = 0:1:p.maxCars
+%                 for returns2 = 0:1:p.maxCars
+%                     
+%                     % Probability for the given combination of rental
+%                     % returns at the two locations
+%                     probReturns = p.returnProbs(returns1+1, returns2+1);
+%                     
+%                     % Update the number of cars at the locations to get the
+%                     % next state
+%                     numCars1 = min(numCars1 + returns1, p.maxCars);
+%                     numCars2 = min(numCars2 + returns2, p.maxCars);
+%                     
+%                     % Total probability of getting this combination of
+%                     % requests and returns at the two locations
+%                     totalProb = probRequests*probReturns;
+%                     
+%                     % Update the state value
+%                     returns = returns + totalProb*(income+...
+%                         p.discountRate*values(numCars1+1, numCars2+1));
+%                 end
+%             end  
 
-    value = reward(problem, nextState, action) + ...
-            problem.discountRate * problem.values(nextState(1)+1,nextState(2)+1);
-end
+            % Update the state value calculation
+            returns1 = p.meanReturns1;
+            returns2 = p.meanReturns2;
+            numCars1 = min(numCars1+returns1, p.maxCars);
+            numCars2 = min(numCars2+returns2, p.maxCars);
+            returns = returns + requestProb*(income+p.discountRate*values(numCars1+1, numCars2+1));
 
-function[val] = reward(problem, state, action)
 
-    reward1 = min(problem.meanRequests(1), state(1));
-    reward2 = min(problem.meanRequests(2), state(2));
-    
-    reward = (reward1 + reward2) * problem.rentProfit;
-    
-    loss = abs(action) * problem.costPerCarMoved;
-    
-    val = reward - loss;
-   
+
+        end
+    end  
 end
